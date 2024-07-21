362aec84df0d2862e3eeffcd16fa79e9d69df0ff
seunghun_5945 <tmdgns5945@oasis.inje.ac.kr>
2024-07-21 17:53:53 +0900

    feat: add social login added

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import LottieAnimation from "../components/LottieAnimation";
 import { RiKakaoTalkFill } from "react-icons/ri";
 import { FcGoogle } from "react-icons/fc";
 import Main from "../components/Main";
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
 
 const Container = styled.div`
   width: 500px;
@@ -76,6 +78,32 @@ const UnderBar = styled.div`
 `;
 
 const SignInContent = () => {
+  useEffect(() => {
+    const naverLogin = new window.naver.LoginWithNaverId({
+      clientId: "KJA8AJCFs2vHONBom5XH",
+      callbackUrl: "http://localhost:3000",
+      isPopup: false,
+      callbackHandle: true,
+      loginButton: { color: "green", type: 3, height: 60 }
+    });
+    naverLogin.init();
+
+    window.addEventListener('load', function () {
+      naverLogin.getLoginStatus(function (status) {
+        if (status) {
+          axios.post('http://localhost:8080/login/oauth2/code/naver', {
+            token: naverLogin.accessToken.accessToken
+          }).then(response => {
+            console.log('Server response:', response.data);
+            // 로그인 성공 후의 동작
+          }).catch(error => {
+            console.error('로그인 실패:', error);
+          });
+        }
+      });
+    });
+  }, []);
+
   return (
     <Container>
       <Frame>
@@ -85,8 +113,22 @@ const SignInContent = () => {
         <Middle><h2>로그인하고 <br/> 노나방으로 자취방 찾으러 가볼까요?</h2></Middle>
         <Footer>
           <LoginArea>
-            <SocialBtn><RiKakaoTalkFill style={{fontSize: "30px", marginRight: "10px"}}/>카카오로 간편 로그인하기</SocialBtn>
-            <SocialBtn style={{backgroundColor: "white"}}><FcGoogle style={{fontSize: "30px", marginRight: "10px"}}/>구글계정으로 간편 로그인하기</SocialBtn>
+            <div id="naverIdLogin"></div>
+            <GoogleOAuthProvider clientId="730599288403-73ughe6q8k2us136drgfn2nf10ke5hq2.apps.googleusercontent.com">
+              <GoogleLogin
+                onSuccess={credentialResponse => {
+                  console.log(credentialResponse);
+                }}
+                onError={() => {
+                  console.log('Login Failed');
+                }}
+                render={renderProps => (
+                  <SocialBtn onClick={renderProps.onClick} disabled={renderProps.disabled} style={{backgroundColor: "white"}}>
+                    <FcGoogle style={{fontSize: "30px", marginRight: "10px"}}/>구글계정으로 간편 로그인하기
+                  </SocialBtn>
+                )}
+              />
+            </GoogleOAuthProvider>
           </LoginArea>
           <LoginArea>
             <span style={{display:"flex", flexDirection: "row", width: "100%", alignItems: "center", justifyContent: "center", color: "lightgray"}}><UnderBar/>또는<UnderBar/></span>
@@ -98,11 +140,11 @@ const SignInContent = () => {
         </Footer>
       </Frame>
     </Container>
-  )
+  );
 }
 
 const SignIn = () => {
-    return <Main props={<SignInContent />} />;
-  };
+  return <Main props={<SignInContent />} />;
+};
 
 export default SignIn;
